1333ddc069014f4445c2e976eb473e97a4df3590
Karim Tarek <cg.karimtarek@gmail.com>
2013-11-10 22:09:21 +0200

    Initial Commit

---
+++ MoonLand/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  MoonLand
+//
+//  Created by Karim on 11/10/13.
+//  Copyright (c) 2013 Helt. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
